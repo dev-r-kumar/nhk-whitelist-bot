8a64bac75f464cff9ee44a5edc6422ba3352f3f2
Rodneel Kumar <168508749+dev-r-kumar@users.noreply.github.com>
2025-11-05 17:14:39 +1200

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -5,9 +5,10 @@ import aiohttp
 import asyncio
 import ssl
 from datetime import datetime
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) # new avoid block cors
 
 whitelist_db_path = os.path.join(os.path.dirname(__file__), "whitelist.json")
 
